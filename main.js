867c37de8cbbb1bbcb765e8452eff11876198c47
zealfire <iamalok185@gmail.com>
2014-04-27 14:47:27 +0530

    again rectified

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 <head>
+
+<script type="text/javascript" src="jquery-2.1.0.js"></script>
+
+<script type="text/javascript" src="Chart.js"></script>
+
 <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
