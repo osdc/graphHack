3edc16b4bbf48699a7d6b1568bae290fd8e7c932
gaurav-arya <gaurav.arya37@gmail.com>
2014-04-27 14:49:56 +0530

    test run

--- main.js
+++ main.js
@@ -3,7 +3,10 @@
  <link rel="stylesheet" type="css/text" href="style.css"/>
    <script type="text/javascript" src="jquery-2.1.0.js"></script>
    <script src="chart.js"></script>
+
 </head>
 <body>
+
+alert("I am an alert box!");
 </body>
 </html>
